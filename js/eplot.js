a164a79496e112053b63d24dd5f4d2f87d7b6fb6
NikoFresh <nicolo.frescura@pm.me>
2024-08-08 10:20:27 +0200

    Wind rose

--- js/eplot.js
+++ js/eplot.js
@@ -55,6 +55,35 @@ fetch("json/day.json")
     // Funzione per estrarre e formattare i dati
     const extractData = key => data[key].map((entry, index) => [labels[index], entry[1]]);
 
+    function processWindData(windDir, windSpeed) {
+      const directions = ['N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW'];
+      const speedRanges = [[0, 5], [5, 10], [10, 15], [15, 20], [20, Infinity]];
+      const data = Array(16).fill().map(() => Array(5).fill(0));
+
+      windDir.forEach((dirEntry, index) => {
+        const speedEntry = windSpeed[index];
+        
+        if (dirEntry && speedEntry && dirEntry[1] !== undefined && speedEntry[1] !== undefined) {
+          const dir = dirEntry[1];
+          const speed = speedEntry[1];
+          
+          if (!isNaN(dir) && !isNaN(speed)) {
+            const dirIndex = Math.round(dir / 22.5) % 16;
+            const speedIndex = speedRanges.findIndex(range => speed >= range[0] && speed < range[1]);
+            
+            if (dirIndex >= 0 && dirIndex < 16 && speedIndex >= 0 && speedIndex < 5) {
+              data[dirIndex][speedIndex]++;
+            }
+          }
+        }
+      });
+
+      return directions.map((dir, i) => ({
+        name: dir,
+        data: speedRanges.map((_, j) => data[i][j])
+      }));
+    }
+
     const [outTemp, outHumidity, dewpoint, windChill, heatIndex, windSpeed, gustSpeed, windDir, barometer, rainRate] = 
       ['outTemp', 'outHumidity', 'dewpoint', 'windchill', 'heatindex', 'windSpeed', 'gustSpeed', 'windDir', 'barometer', 'rainrate']
         .map(extractData);
@@ -104,12 +133,6 @@ fetch("json/day.json")
       ]
     });
 
-    initChart("windDirChart", {
-      ...createBaseChartOption(["Direzione"], "{value} °"),
-      yAxis: { ...createBaseChartOption(["Direzione"], "{value} °").yAxis, max: 360 },
-      series: [createSeries("Direzione", windDir, "rgb(192, 75, 192)", "scatter")]
-    });
-
     initChart("heatIndexChart", {
       ...createBaseChartOption(["Heat index", "Wind chill"], "{value} °C"),
       series: [
@@ -121,5 +144,35 @@ fetch("json/day.json")
     initChart("barometerChart", {
       ...createBaseChartOption(["Pressione"], "{value} mbar"),
       series: [createSeries("Pressione", barometer, "rgb(75, 192, 192)")]
+    });
+
+    const windRoseData = processWindData(windDir, windSpeed);
+    initChart("windDirChart", {
+       tooltip: {
+        trigger: 'item',
+        formatter: function(params) {
+          const speedRanges = ['0-5', '5-10', '10-15', '15-20', '20+'];
+          const speedIndex = params.componentIndex;
+          return `${params.name}<br/>${speedRanges[speedIndex]} km/h`;
+        }
+      },
+      legend: {
+        data: ['0-5 km/h', '5-10 km/h', '10-15 km/h', '15-20 km/h', '20+ km/h']
+      },
+      polar: {},
+      angleAxis: {
+        type: 'category',
+        data: windRoseData.map(item => item.name),
+        startAngle: 101, // Inizia da Nord (90 gradi)
+        clockwise: true // Ruota in senso antiorario
+      },
+      radiusAxis: {},
+      series: [0, 1, 2, 3, 4].map(i => ({
+        type: 'bar',
+        data: windRoseData.map(item => item.data[i]),
+        coordinateSystem: 'polar',
+        name: ['0-5 km/h', '5-10 km/h', '10-15 km/h', '15-20 km/h', '20+ km/h'][i],
+        stack: 'stack'
+      }))
     });
   });
