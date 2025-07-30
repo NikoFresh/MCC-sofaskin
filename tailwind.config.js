f5efadcddf6650955d00fa3ab15029cfa91e33a8
NikoFresh <nicolo.frescura@pm.me>
2025-07-30 15:56:24 +0200

    Upgrade to tailwind 4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  prefix: "tw-",
+  prefix: "tw",
   content: ["./*.html", "./js/*.js", "./*.html.tmpl"],
   theme: {
     container: {
       padding: {
-        DEFAULT: '0rem',
-        sm: '0.5rem',
-        lg: '2rem',
-        xl: '3rem',
-        '2xl': '4rem',
+        DEFAULT: "0rem",
+        sm: "0.5rem",
+        lg: "2rem",
+        xl: "3rem",
+        "2xl": "4rem",
       },
     },
     extend: {
       fontFamily: {
-        "Robot-mono": ['"Robot Mono"', "monospace"],
+        "Roboto-mono": ['"Roboto Mono"', "monospace"],
       },
     },
   },
   plugins: [],
 };
-
+// npx @tailwindcss/cli -c tailwind.config.js -i ./tailwind.css -o ./build.css --minify
